473df821323aac695c217ebbc184193a82b4394f
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-05-16 20:59:19 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+//! # A library to use Apple frameworks in Rust
+#![deny(
+    bad_style,
+    const_err,
+    dead_code,
+    improper_ctypes,
+    missing_debug_implementations,
+    missing_docs,
+    no_mangle_generic_items,
+    non_shorthand_field_patterns,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_in_public,
+    trivial_casts,
+    trivial_numeric_casts,
+    unconditional_recursion,
+    unused_allocation,
+    unused_comparisons,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_parens,
+    unused_qualifications,
+    unused_results,
+    unused,
+    while_true
+)]
+#![warn(clippy::all, missing_debug_implementations)]
