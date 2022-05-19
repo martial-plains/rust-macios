a507345e224e0e7a842090a918ba0ce12dc582fd
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-05-19 16:10:35 -0400

    Add Swift-like type aliases

--- src/foundation/numbers_data_and_basic_values.rs
+++ src/foundation/numbers_data_and_basic_values.rs
@@ -14,5 +14,29 @@ pub type Int = libc::c_long;
 #[cfg(target_pointer_width = "64")]
 pub type UInt = libc::c_ulong;
 
+/// Describes an 8-bit signed integer.
+pub type Int8 = libc::c_schar;
+
+/// Describes an 8-bit unsigned integer.
+pub type UInt8 = libc::c_uchar;
+
+/// Describes a 16-bit signed integer.
+pub type Int16 = libc::c_short;
+
+/// Describes a 16-bit unsigned integer.
+pub type UInt16 = libc::c_ushort;
+
+/// Describes a 32-bit signed integer.
+pub type Int32 = libc::c_int;
+
+/// Describes a 32-bit unsigned integer.
+pub type UInt32 = libc::c_uint;
+
+/// Describes a 64-bit signed integer.
+pub type Int64 = libc::c_longlong;
+
+/// Describes a 64-bit unsigned integer.
+pub type UInt64 = libc::c_ulonglong;
+
 /// A double-precision, floating-point value type.
 pub type Double = libc::c_double;
