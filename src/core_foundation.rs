2bb6c251f490f01cecb3428e5a6653e949ec7775
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-06-06 12:28:11 -0400

    Add CFTypeID

---
+++ src/core_foundation.rs
@@ -0,0 +1,6 @@
+//! Access low-level functions, primitive data types, and various collection
+//! types that are bridged seamlessly with the Foundation framework.
+
+mod type_defs;
+
+pub use type_defs::*;
---
+++ src/core_foundation/type_defs.rs
@@ -0,0 +1,4 @@
+use libc::c_ulong;
+
+/// A type for unique, constant integer values that identify particular Core Foundation opaque types.
+pub type CFTypeID = c_ulong;
