6c5cb496bc41a46a134e81c45444db1aa9ce567c
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-06-07 03:46:32 -0400

    Add Docs

--- src/appkit/enums.rs
+++ src/appkit/enums.rs
@@ -1,7 +1,12 @@
+/// Constants that indicate whether a copy or print operation was successful,
+/// was canceled, or failed.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u64)]
 pub enum NSApplicationDelegateReply {
+    /// Indicates the operation succeeded.
     Success = 0,
+    /// Indicates the user cancelled the operation.
     Cancel = 1,
+    /// Indicates an error occurred processing the operation.
     Failure = 2,
 }
