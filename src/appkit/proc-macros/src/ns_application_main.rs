4d57f07862eb964c99569f0359627ce4c825f582
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-06-13 15:02:38 -0400

    Fix NSApplicationMain macro

--- src/appkit/proc-macros/src/ns_application_main.rs
+++ src/appkit/proc-macros/src/ns_application_main.rs
@@ -5,21 +5,31 @@ use syn::{parse_macro_input, ItemFn};
 pub fn entry_point(item: TokenStream) -> TokenStream {
     let item = parse_macro_input!(item as ItemFn);
 
-    let block = *item.block;
+    let stms = item.block.stmts;
 
-    quote! {
+    let usings = quote! {
         use rust_macios::appkit::NSApplicationMain;
         use std::os::raw::c_int;
+    };
 
-        #[allow(dead_code)]
+    let user_code = quote! {
+        #(#stms)*
+    };
+
+    let main_func = quote! {
+        #usings
         fn main() {
+            #user_code
             unsafe {
-                #block
                 let args = std::env::args();
                 let argv = &args as *const _ as *const *const i8;
                 NSApplicationMain(args.len() as c_int, argv);
             }
         }
+    };
+
+    quote! {
+        #main_func
     }
     .into()
 }
