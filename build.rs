4df513c3266ca301b10f5a5c7c76ae6555620289
Allister Isaiah Harvey <a.i.harvey@icloud.com>
2022-10-02 15:46:30 -0400

    Add conditional compilation for linking

--- build.rs
+++ build.rs
@@ -1,19 +1,35 @@
 fn main() {
     let target = std::env::var("TARGET").unwrap();
 
-    if target.contains("-ios") {
+    if target.contains("apple-ios") {
+        #[cfg(feature = "uikit")]
         println!("cargo:rustc-link-lib=framework=UIKit");
-    } else {
+    } else if target.contains("apple-darwin") {
+        #[cfg(feature = "appkit")]
         println!("cargo:rustc-link-lib=framework=AppKit");
     }
 
+    #[cfg(feature = "user_notifications")]
     println!("cargo:rustc-link-lib=framework=UserNotifications");
 
+    #[cfg(feature = "foundation")]
     println!("cargo:rustc-link-lib=framework=Foundation");
+
+    #[cfg(feature = "compression")]
     println!("cargo:rustc-link-lib=dylib=Compression");
+
+    #[cfg(feature = "natural_language")]
     println!("cargo:rustc-link-lib=framework=NaturalLanguage");
+
+    #[cfg(feature = "background_tasks")]
     println!("cargo:rustc-link-lib=framework=BackgroundTasks");
+
+    #[cfg(feature = "core_graphics")]
     println!("cargo:rustc-link-lib=framework=CoreGraphics");
+
+    #[cfg(feature = "contacts")]
     println!("cargo:rustc-link-lib=framework=Contacts");
+
+    #[cfg(feature = "core_ml")]
     println!("cargo:rustc-link-lib=framework=CoreML");
 }
